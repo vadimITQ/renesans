fc5d6d909249983db5d8af18cd7500bba45581a5
VTB4071707@region.vtb.ru <dandruschak@vtb.ru>
2023-03-03 16:35:25 +0300

    lessThanDateDiapason validator implementation

--- src/app/shared/validation/validators.ts
+++ src/app/shared/validation/validators.ts
@@ -1,23 +1,46 @@
-import { isBefore, isAfter, parse } from 'date-fns';
+import { isBefore, isAfter, parse, differenceInCalendarDays } from 'date-fns';
 import { ValidationMessage } from './types';
 import { dateFormat, dateFormatWithTime } from '../components/controls/date-picker/date-picker.constants';
 
-export function earlierThen(from: string | null, to: string | null): ValidationMessage {
+function parseDates(from: string | null, to: string | null): { fromDate: Date; toDate: Date } | null {
   if (!from || !to) {
     return null;
   }
 
   const fromDate = parse(from, from.includes(' ') ? dateFormatWithTime : dateFormat, new Date());
   const toDate = parse(to, to.includes(' ') ? dateFormatWithTime : dateFormat, new Date());
+  return { fromDate, toDate };
+}
+
+export function earlierThen(from: string | null, to: string | null): ValidationMessage {
+  const parsedDates = parseDates(from, to);
+
+  if (!parsedDates) {
+    return null;
+  }
+
+  const { fromDate, toDate } = parsedDates;
   return !isBefore(fromDate, toDate) ? `Должно быть раньше, чем ${to}` : null;
 }
 
 export function laterThen(from: string | null, to: string | null): ValidationMessage {
-  if (!from || !to) {
+  const parsedDates = parseDates(from, to);
+
+  if (!parsedDates) {
     return null;
   }
 
-  const fromDate = parse(from, from.includes(' ') ? dateFormatWithTime : dateFormat, new Date());
-  const toDate = parse(to, to.includes(' ') ? dateFormatWithTime : dateFormat, new Date());
+  const { fromDate, toDate } = parsedDates;
   return !isAfter(toDate, fromDate) ? `Должно быть позже, чем ${from}` : null;
+}
+
+export function lessThanDateDiapason(from: string | null, to: string | null, diapason: number): ValidationMessage {
+  const parsedDates = parseDates(from, to);
+
+  if (!parsedDates) {
+    return null;
+  }
+
+  const { fromDate, toDate } = parsedDates;
+  return differenceInCalendarDays(toDate, fromDate) > diapason ? `Диапозон дат не должен превышать ${diapason} дней.` : null;
 }

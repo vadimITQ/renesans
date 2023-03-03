0c2603b99a37ad628b3389b5c109c91d7f75c177
kanzaki <urkanzak1@mail.ru>
2023-03-03 16:07:09 +0300

    123

--- src/app/core/pages/PE/search-payment/search-payment-table/search-payment-table.component.ts
+++ src/app/core/pages/PE/search-payment/search-payment-table/search-payment-table.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { searchPaymentTableColumns } from './search-payment-table.constants';
 import { searchPaymentMock } from '../search-payment.mock';
+import { XlsxHelper } from 'src/app/shared/classes/xlsx-Helper';
 
 @Component({
   selector: 'app-search-payment-table',
@@ -14,4 +15,9 @@ export class SearchPaymentTableComponent {
   onRowSelected(e: any) {
     console.log(e);
   }
+
+  generateReport() {
+    XlsxHelper.exportArrayToExcel(searchPaymentMock, Object.getOwnPropertyNames(searchPaymentMock[0]), "Выгрузка_в_excel_test");
+  }
+
 }

ee992b6650a0f878b7c0ecac14a269ec7f4898b1
kanzaki <urkanzak1@mail.ru>
2023-03-14 15:15:38 +0300

    123

--- src/app/shared/validation/validators.ts
+++ src/app/shared/validation/validators.ts
@@ -1,6 +1,7 @@
-import { isBefore, isAfter, parse, differenceInCalendarDays } from 'date-fns';
+import { isBefore, isAfter, parse, differenceInCalendarDays, parseISO } from 'date-fns';
 import { ValidationMessage } from './types';
 import { dateFormat, dateFormatWithTime } from '../components/controls/date-picker/date-picker.constants';
+import { DatePickerHelper } from '../components/controls/date-picker/date-picker-helper';
 
 function parseDates(from: string | null, to: string | null): { fromDate: Date; toDate: Date } | null {
   if (!from || !to) {
@@ -13,36 +14,37 @@ function parseDates(from: string | null, to: string | null): { fromDate: Date; t
 }
 
 export function earlierThen(from: string | null, to: string | null): ValidationMessage {
-  const parsedDates = parseDates(from, to);
+  const parsedDates = [DatePickerHelper.convertToDate(from), DatePickerHelper.convertToDate(to)];
 
   if (!parsedDates) {
     return null;
   }
 
-  const { fromDate, toDate } = parsedDates;
-  return !isBefore(fromDate, toDate) ? `Должно быть раньше, чем ${to}` : null;
+  const [ fromDate, toDate ] = parsedDates;
+
+  return !isBefore(fromDate ?? 0, toDate ?? 0) ? `Должно быть раньше, чем ${to}` : null;
 }
 
 export function laterThen(from: string | null, to: string | null): ValidationMessage {
-  const parsedDates = parseDates(from, to);
+  const parsedDates = [DatePickerHelper.convertToDate(from), DatePickerHelper.convertToDate(to)];
 
   if (!parsedDates) {
     return null;
   }
 
-  const { fromDate, toDate } = parsedDates;
-  return !isAfter(toDate, fromDate) ? `Должно быть позже, чем ${from}` : null;
+  const [ fromDate, toDate ] = parsedDates;
+  return !isAfter(toDate ?? 0, fromDate ?? 0) ? `Должно быть позже, чем ${from}` : null;
 }
 
 export function lessThanDateDiapason(from: string | null, to: string | null, diapason: number): ValidationMessage {
-  const parsedDates = parseDates(from, to);
+  const parsedDates = [DatePickerHelper.convertToDate(from), DatePickerHelper.convertToDate(to)];
 
   if (!parsedDates) {
     return null;
   }
 
-  const { fromDate, toDate } = parsedDates;
-  return differenceInCalendarDays(toDate, fromDate) > diapason ? `Диапозон дат не должен превышать ${diapason} дней.` : null;
+  const [ fromDate, toDate ] = parsedDates;
+  return differenceInCalendarDays(toDate ?? 0, fromDate ?? 0) > diapason ? `Диапозон дат не должен превышать ${diapason} дней.` : null;
 }
 
 export function required(value: unknown): ValidationMessage {

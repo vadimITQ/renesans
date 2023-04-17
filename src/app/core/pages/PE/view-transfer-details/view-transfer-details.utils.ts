1e6c691421cd547cac64019e2456697afa9d7b25
Vadim <dandruschak@vtb.ru>
2023-04-17 18:37:52 +0300

    fixed PE-3325

--- src/app/core/pages/PE/view-transfer-details/view-transfer-details.utils.ts
+++ src/app/core/pages/PE/view-transfer-details/view-transfer-details.utils.ts
@@ -1,9 +1,15 @@
-import {ITransferDetails} from "../../../services/view-transfer-details/types";
-import {dateFormatWithTime} from "../../../../shared/components/controls/date-picker/date-picker.constants";
+import { ITransferDetails } from '../../../services/view-transfer-details/types';
+import { dateFormatWithTime } from '../../../../shared/components/controls/date-picker/date-picker.constants';
 import { format } from 'date-fns';
+import {paymentStatusObj} from "../../../../shared/variables/payment-status";
 
-
-export function prepareTransferDetails(transferDetails:ITransferDetails): ITransferDetails {
-  return {...transferDetails, statusHistory: transferDetails.statusHistory.map((value)=>({...value,
-      statusChangeTime: format(new Date(value.statusChangeTime), dateFormatWithTime)}))}
+export function prepareTransferDetails(transferDetails: ITransferDetails): ITransferDetails {
+  return {
+    ...transferDetails,
+    statusHistory: transferDetails.statusHistory.map(value => ({
+      ...value,
+      statusChangeTime: format(new Date(value.statusChangeTime), dateFormatWithTime),
+      statusComment:paymentStatusObj[value.codeStatus ?? '']
+    })),
+  };
 }

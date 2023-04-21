ef9fbc5b3602488e52008c8755243e93f6b63564
kanzaki <urkanzak1@mail.ru>
2023-04-21 14:56:29 +0300

    reactive forms: pe-r-input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
 import { PrimeNGConfig } from 'primeng/api';
+import { PEValidators } from 'src/app/shared/components/reactive-controls/validators';
+import { messages, ErrorMesssagesList } from './shared/components/reactive-controls/global-error-messages';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   
   title = 'PE';
 
-  constructor(private translateService: TranslateService, private config: PrimeNGConfig){
+  filter: FormGroup = this.createFormGroup();
+
+  errorMessages: ErrorMesssagesList = messages.formGroupMessages.global;
+
+  createFormGroup(){
+    return this.fb.group({
+      paymentID: new FormControl("", PEValidators.ManualChecksFilterValidators.PeInputValidators.FormControlValidators.Required),
+      applicationID: new FormControl("", PEValidators.ManualChecksFilterValidators.PeInputValidators.FormControlValidators.Required),
+      paymentHubPaymentId: new FormControl("", PEValidators.ManualChecksFilterValidators.PeInputValidators.FormControlValidators.Required),
+      account: new FormControl("", PEValidators.ManualChecksFilterValidators.PeInputValidators.FormControlValidators.Required)
+    },{
+      validators: PEValidators.ManualChecksFilterValidators.PeInputValidators.FormGroupValidators.Required
+    });
+  }
+
+  constructor(private translateService: TranslateService, private config: PrimeNGConfig, private fb: FormBuilder){
     this.translateService.setDefaultLang("ru");
     this.translateService.use("ru");
     this.translateService
@@ -21,6 +39,24 @@ export class AppComponent {
           this.config.setTranslation(res);
         }
       );
+  }
+
+  ngOnInit(): void {
+    this.filter.valueChanges.subscribe(console.log);
+  }
+
+  submit(){
+    this.filter.markAsDirty();  
+    this.filter.markAsTouched();
+    this.filter.controls["paymentID"].markAsDirty();
+    this.filter.controls["applicationID"].markAsDirty();
+    this.filter.controls["paymentHubPaymentId"].markAsDirty();
+    this.filter.controls["account"].markAsDirty();
+    this.filter.controls["paymentID"].markAsTouched();
+    this.filter.controls["applicationID"].markAsTouched();
+    this.filter.controls["paymentHubPaymentId"].markAsTouched();
+    this.filter.controls["account"].markAsTouched();
+    
   }
 
 }

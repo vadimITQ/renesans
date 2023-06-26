41a9a4a9676ff344e524de8b72824fadb631a9de
kanzaki <urkanzak1@mail.ru>
2023-06-26 10:47:29 +0300

    PE-5646

--- src/app/core/pages/PE/anti-fraud-check/anti-fraud-check-filter/anti-fraud-checks-filter.utils.ts
+++ src/app/core/pages/PE/anti-fraud-check/anti-fraud-check-filter/anti-fraud-checks-filter.utils.ts
@@ -1,6 +1,7 @@
 
 import { subDays } from "date-fns";
 import { AntiFraudCheckFilter } from "./anti-fraud-checks-filter.types";
+import { IMultiSelectData } from "src/app/shared/components/controls/pe-multiselect/pe-multiselect.component";
 
 export class AntiFraudChecksFilterUtils {
 
@@ -8,13 +9,17 @@ export class AntiFraudChecksFilterUtils {
 
         const dateTo = new Date();
         const dateFrom = subDays(dateTo, 3);
+        const applicationStatus: IMultiSelectData[] = [{
+            label: "Ожидает рассмотрения AntiFraud",
+            value: "Ожидает рассмотрения AntiFraud"
+        }];
         
         return  {
             IdPE: "",
             applicationId: "",
             dateFrom: dateFrom,
             dateTo: dateTo,
-            applicationStatus: [],
+            applicationStatus: applicationStatus,
             onlyExpired: false
         }
     }
